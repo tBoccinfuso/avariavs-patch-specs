d20c47f0c8b54ad10800d25b92bff29528a2c52a
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-05-11 16:54:49 -0500

    updated with Pre beta # 3 (v0.4)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Replace the following string with patch version folder and file.
-const csvFilePath = './Patches-csv/v0.3/AVARIAvs public specs - soldier_Abilities.csv';
+const csvFilePath = './Patches-csv/v0.4/AVARIAvs public specs - heroes.csv';
 const csv=require('csvtojson');
 const fs = require('fs');
 
@@ -8,10 +8,10 @@ csv()
 .fromFile(csvFilePath)
 .on('json',(jsonObj)=>{
     // Change version folder.
-    fs.appendFile('./json/v.03' + csvFilePath.slice(21) + '.json', JSON.stringify(jsonObj, null, 4) ,function (err) {
+    fs.appendFile('./json/' + csvFilePath.slice(19) + '.json', JSON.stringify(jsonObj, null, 4) ,function (err) {
         if (err) throw err;
     });
 })
 .on('done',(error)=>{
-    console.log('end');
+    console.log('converted file');
 });
